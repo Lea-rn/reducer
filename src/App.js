f0b13f9ca2e2229a0f72269989ed574517bb5a16
achrefmechergui <achreffcb123achref@gamil.com>
2026-01-19 20:00:03 +0100

    reducer

--- src/App.js
+++ src/App.js
@@ -1,59 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  console.log(state, action);
+  if (action.type === "inc") return {...state , count : state.count +1 }
+  if (action.type === "dec") return {...state , count : state.count -1}
+  if (action.type === "setCount") return action.payload
+  
+}
 
 function App() {
-  const [count , setCount] = useState(0) ; 
-  const [step , setStep] = useState(1) ; 
+  // const [count , setCount] = useState(0) ;
+    // const [step, setStep] = useState(1);
+    const initialState = {count : 0 , step : 1}
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const date = new Date ("june 16 2025")
-  date.setDate(date.getDate() + count)  
+  const {count , step} = state
+  const date = new Date("june 16 2025");
+  date.setDate(date.getDate() + count);
 
+  const dec = function () {
+    // setCount((cur)=> cur - step)
+    dispatch({type : "dec" })
+  };
 
-  const dec = function (){
-    setCount((cur)=> cur - step)
-  }
+  const inc = function () {
+    // setCount ((cur)=> cur + step)
+    dispatch({type : "inc" });
+  };
 
-   
-  const inc = function (){
-    setCount ((cur)=> cur + step)
-  }
+  const definecount = function (e) {
+    // setCount(Number(e.target.value))
+    dispatch({type : "setCount" , payload : Number(e.target.value)})
+    
+  };
 
-  const definecount = function (e){
-    setCount(Number(e.target.value))
-  }
+  const defineStep = function (e) {
+    // setStep(Number(e.target.value));
+  };
 
-  const defineStep = function (e){
-  setStep(Number(e.target.value))
-  }
-
-  const reset = function (){
-    setCount(0)
-    setStep(1)
-  }
+  const reset = function () {
+    // setCount(0)
+    // setStep(1);
+  };
 
   return (
     <div className="App">
-     <div>
-      <input
-      type='range'
-      min="0"
-      max="10"
-      value={step}
-      onChange={defineStep}
-      />
-      <span>{step}</span>
-     </div>
-     <div>
-      <button onClick={dec}>-</button>
-      <input value={count} onChange={definecount}/>
-      <button onClick={inc}>+</button>
-     </div>
-     <p>{date.toDateString()}</p>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={defineStep}
+        />
+        <span>{step}</span>
+      </div>
+      <div>
+        <button onClick={dec}>-</button>
+        <input value={count} onChange={definecount} />
+        <button onClick={inc}>+</button>
+      </div>
+      <p>{date.toDateString()}</p>
 
-     <div>
-      <button onClick={reset}>Reset</button>
-     </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
     </div>
   );
 }
